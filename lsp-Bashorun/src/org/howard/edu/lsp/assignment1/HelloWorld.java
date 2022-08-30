1452285b8918001b15195d32d06d4b4ea4598ea1
sucrepapitoo <sucrepapitoo@172.17.13.27>
2022-08-30 01:45:45 -0400

    This is my commit for assignment #1

---
+++ lsp-Bashorun/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+/**
+ * Oluwasubomi Bashorun
+ * @author sucrepapitoo
+ * 
+ * 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
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!!");
+	}
+
+}
