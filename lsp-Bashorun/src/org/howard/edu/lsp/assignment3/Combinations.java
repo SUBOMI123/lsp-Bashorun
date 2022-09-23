7465c00ac690a5259d9eef0de0cebeba6863592d
sucrepapitoo <sucrepapitoo@172.17.13.27>
2022-09-22 23:37:23 -0400

    This is my commit for Assignment 3

--- lsp-Bashorun/src/org/howard/edu/lsp/assignment3/Combinations.java
+++ lsp-Bashorun/src/org/howard/edu/lsp/assignment3/Combinations.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class Combinations {
 
 	// Method to find the sets with given sum
-    public void calculate_combinations(ArrayList<ArrayList<Integer>> answer, 
+    public void calculate_combinations(ArrayList<ArrayList<Integer>> result, 
         int input[], 
-        ArrayList<Integer> tmp, 
+        ArrayList<Integer> temp, 
         int sum, 
         int start) {
     	
@@ -21,26 +21,26 @@ public class Combinations {
 
 		// if(curr_target == 0) means that the sum of this set is equal to target
 		if(sum == 0) {
-        	answer.add(new ArrayList<Integer>(tmp));
+        	result.add(new ArrayList<Integer>(temp));
 			return;
 		} else {
 			for (int i = start; i<input.length; i++) {
                 // Add the value to the temporary answer
-				tmp.add(i);
+				temp.add(i);
 
                 // Decrease the curr_target by substracting added value
-				calculate_combinations(answer, input, tmp, sum - input[i], i+1);
+				calculate_combinations(result, input, temp, sum - input[i], i+1);
 
                 // Backtrack and remove the temporary added value from answer
-				tmp.remove(tmp.size()-1);
+				temp.remove(temp.size()-1);
 			}
 		}
     }
     
     // Prints the indexes in each set on a new line
-    public void printSets(ArrayList<ArrayList<Integer>> answer) {
+    public void printSets(ArrayList<ArrayList<Integer>> result) {
 
-        int n = answer.size();
+        int n = result.size();
         
         if(n == 0) {
         	System.out.println("[]");
@@ -51,11 +51,11 @@ public class Combinations {
         // Loop to print the subsequences
 		for(int i = 0; i<n; i++){
 
-            int m = answer.get(i).size();
+            int m = result.get(i).size();
             System.out.print("[");
 
 			for(int j = 0; j<m ; j++){
-                System.out.print(answer.get(i).get(j));
+                System.out.print(result.get(i).get(j));
                 
                 if(j != m-1)
 				    System.out.print(", ");
--- lsp-Bashorun/src/org/howard/edu/lsp/assignment3/Driver.java
+++ lsp-Bashorun/src/org/howard/edu/lsp/assignment3/Driver.java
@@ -5,59 +5,58 @@ import java.util.ArrayList;
 import java.util.Arrays; 
 
 public class Driver {
-    // Driver method
     public static void main(String[] args) {
 
         // Create combination object
         Combinations comb = new Combinations();
         // Stores the answer
-        ArrayList<ArrayList<Integer>> answer = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 
         // Declare array 1
         int arr1[] = {5, 5, 15, 10};
         int target1 = 15;    // Assign target sum
     
         // Find the sets and store them in the answer
-        comb.calculate_combinations(answer, arr1, new ArrayList<Integer>(), target1, 0);
+        comb.calculate_combinations(result, arr1, new ArrayList<Integer>(), target1, 0);
         // Print the answers
         System.out.println("Input: "+ Arrays.toString(arr1)+" Sum: "+ target1);
         System.out.println("Combinations: ");
-        comb.printSets(answer);
+        comb.printSets(result);
 
         // Declare array 2
         int arr2[] = {1, 2, 3, 4, 5, 6};
         int target2 = 6;    // Assign target sum
-        answer.clear();     // Clear answer
+        result.clear();     // Clear answer
 
         // Find the sets and store them in the answer
-        comb.calculate_combinations(answer, arr2, new ArrayList<Integer>(), target2, 0);
+        comb.calculate_combinations(result, arr2, new ArrayList<Integer>(), target2, 0);
         // Print the answers
         System.out.println("Input: "+ Arrays.toString(arr2)+" Sum: "+ target2);
         System.out.println("Combinations: ");
-        comb.printSets(answer);
+        comb.printSets(result);
         
      // Declare array 3
         int arr3[] = {};
         int target3 = 15;    // Assign target sum
-        answer.clear();     // Clear answer
+        result.clear();     // Clear answer
 
         // Find the sets and store them in the answer
-        comb.calculate_combinations(answer, arr3, new ArrayList<Integer>(), target3, 0);
+        comb.calculate_combinations(result, arr3, new ArrayList<Integer>(), target3, 0);
         // Print the answers
         System.out.println("Input: "+ Arrays.toString(arr3)+" Sum: "+ target3);
         System.out.println("Combinations: ");
-        comb.printSets(answer);
+        comb.printSets(result);
         
      // Declare array 4
         int arr4[] = {2,2,2};
         int target4 = 5;    // Assign sum
-        answer.clear();     // Clear answer
+        result.clear();     // Clear answer
 
         // Find the sets and store them in the answer
-        comb.calculate_combinations(answer, arr4, new ArrayList<Integer>(), target4, 0);
+        comb.calculate_combinations(result, arr4, new ArrayList<Integer>(), target4, 0);
         // Print the answers
         System.out.println("Input: "+ Arrays.toString(arr4)+" Sum: "+ target4);
         System.out.println("Combinations: ");
-        comb.printSets(answer);
+        comb.printSets(result);
     }
 }
