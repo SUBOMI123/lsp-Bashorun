21dbfbbcd4b134452de97c0759dfb7d6aba00b7b
sucrepapitoo <sucrepapitoo@172.17.13.27>
2022-10-17 16:56:49 -0400

    This is my commit for assignment 5

---
+++ lsp-Bashorun/src/org/howard/edu/lsp/assignment5/IntegerSetException.java
@@ -0,0 +1,20 @@
+package org.howard.edu.lsp.assignment5;
+
+/**
+ * 
+ * @author sucrepapitoo
+ *
+ */
+public class IntegerSetException extends Exception {
+	private static final long serialVersionUID = 6585988606273525868L;
+/**
+ * 
+ * @param str, 
+ */
+	public IntegerSetException(String str)  
+    {  
+        // Call the constructor of parent exception
+        super(str); 
+    }  
+
+}
