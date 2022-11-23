1ad8709bf4189bcaee698f56867a0437e1365949
SUBOMI123 <79486152+SUBOMI123@users.noreply.github.com>
2022-11-23 13:56:08 -0500

    Create ClassGradeAverage.java
    Commit for assignment 7

---
+++ lsp-Bashorun/src/org/howard/edu/lsp/assignment7/ClassGradeAverage.java
@@ -0,0 +1,38 @@
+package org.howard.edu.lsp.assignment7;
+
+import java.util.Collections;
+import java.util.List;
+/**
+ * 
+ * @author sucrepapitoo
+ *
+ */
+class ClassGradeAverage implements AverageStrategy {
+    @Override
+    /**
+	   * This returns the average grades from a list of integer number
+	   * @param grades, takes in a list of integer grades
+	   * @return average of the grades 
+	   * @throws EmptyListException "list is empty"
+	   */
+    public int compute(List<Integer> grades) throws EmptyListException {
+            if(grades == null || grades.size() < 1) {
+                    throw new EmptyListException();
+            }
+
+            int counter = 0;
+            int sum = 0;
+            int res = 0;
+            int n = grades.size();
+            Collections.sort(grades);
+            for(int i=0; i<n; i++) {
+                    sum += grades.get(i);
+                    counter++;
+            }
+            res = sum/counter;
+            return res;
+    }
+}
+    
+
+    
